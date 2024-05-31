e823891b4203d23e80b59855104bdbbb199de7a6
Sekhar2004 <jeerurajasekharreddy@gmail.com>
2024-05-31 11:48:43 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="notes-app__footer">
+      <p>Made with ❤️</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/utils/data.js
@@ -0,0 +1,15 @@
+const getInitialData = () => ([
+    
+  ]);
+  
+  const showFormattedDate = (date) => {
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    }
+    return new Date(date).toLocaleDateString("en-US", options)
+  }
+  
+  export { getInitialData, showFormattedDate };
